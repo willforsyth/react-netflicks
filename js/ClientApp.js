fafaeaefa6f24b3796c24e7d5e2db7d8721482d7
Will Forsyth <will.forsyth@drummondcentral.co.uk>
2016-08-23 17:05:47 +0100

    initial commit

---
+++ js/ClientApp.js
@@ -0,0 +1,15 @@
+var React = require('react')
+var ReactDOM = require('react-dom')
+var MyTitle = require('./MyTitle')
+
+var div = React.DOM.div
+
+
+var MyFirstComponent = (
+  div(null,
+    React.createElement(MyTitle, {title: 'Use props everywhere'}),
+    React.createElement(MyTitle, {title: 'Use props everywhere!!!!'})
+  )
+)
+
+ReactDOM.render(MyFirstComponent, document.getElementById('app'))
---
+++ js/MyTitle.js
@@ -0,0 +1,16 @@
+var React = require ('react')
+
+var div = React.DOM.div
+var h1 = React.DOM.h1
+
+var MyTitle = React.createClass({
+  render () {
+    return (
+      div(null,
+        h1(null, this.props.title)
+      )
+    )
+  }
+})
+
+module.exports = MyTitle
